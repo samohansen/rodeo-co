cf6ef8618a618f5b72b0f3c887ab3d1157241c48
17gfillmore <gracefillmore8@gmail.com>
2023-02-06 23:06:13 -0700

    HOORAY :) (the app layout works now; I guess node was outdated and it was breaking it somehow?)

--- src/common/navigation/MainToolbar.tsx
+++ src/common/navigation/MainToolbar.tsx
@@ -1,14 +1,12 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { useContext } from 'react';
-import {CrumbsContext} from 'src/features/CrumbsContext';
 
 type Props = {
   leftNavWidth: number;
 }
 
 const MainToolbar: React.FC<Props> = ({leftNavWidth}) => {
-  const crumbs = useContext(CrumbsContext);
 
   return <>
     <AppBar
@@ -16,7 +14,7 @@ const MainToolbar: React.FC<Props> = ({leftNavWidth}) => {
       sx={{ width: `calc(100% - ${leftNavWidth}px)`, ml: `${leftNavWidth}px` }}
     >
       <Toolbar>
-        {crumbs}
+        breadcrumbs?
       </Toolbar>
     </AppBar>
   </>
