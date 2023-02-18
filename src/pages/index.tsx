a4c167c8b90f8ef300fb89557f07e98d4f0da8ff
Samuel Hansen <samuelhansen16@gmail.com>
2023-02-18 16:28:37 -0700

    Added Poppins font, added some Home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div></div>
+    <>
+      <h1>🏠 Home</h1>
+      <p>Welcome home, my child. This is here just to help me know if I properly routed the user to home.</p>
+    </>
   );
 };
 
