1710c6944b3a988c7df90ea724c3f350ec564210
Samuel Hansen <samuelhansen16@gmail.com>
2023-03-21 08:53:09 -0600

    nextauth fixes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,7 +18,7 @@ const options = {
     })
   ],
   adapter: PrismaAdapter(prisma),
-  // secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
