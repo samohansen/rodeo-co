83ca27c4ef372855f4c1f45e41418ffd25194c3e
haileyhullinger <hh49362@gmail.com>
2023-03-21 10:40:26 -0600

    random image assigned to rodeo

--- src/features/RodeoDashboard/RodeoCard.tsx
+++ src/features/RodeoDashboard/RodeoCard.tsx
@@ -1,6 +1,7 @@
 import ActionAreaCard from "@common/dataDisplay/ActionAreaCard";
 
 const RodeoCard = ({rodeo, onClick}) => {
+  const rodeoImages = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png']
   const {name, location, date} = rodeo
 
   return (
@@ -9,6 +10,7 @@ const RodeoCard = ({rodeo, onClick}) => {
       bodyLine1={location}
       bodyLine2={date}
       onClick={onClick}
+      imagePath={rodeoImages[Math.floor(Math.random() * 9)]}
     />
   )
 }
