2250c1b52bf42c72a5620b73659cfe8be38c5ac0
haileyhullinger <hh49362@gmail.com>
2023-03-22 10:27:23 -0600

    Home page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,18 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { Box, Button, Container, Typography } from '@mui/material';
 import {useSession, signOut} from 'next-auth/react';
-import marketing1 from 'public/marketing1.png';
-import marketing2 from 'public/marketing2.png';
-import marketing3 from 'public/marketing3.png';
-import cubra from 'public/cubra.png';
+import header from 'public/header.png';
+import body from 'public/body.png';
+import tradition from 'public/tradition.png';
 import type { NextPageWithLayout } from '@common/types';
 import type { ReactElement } from 'react';
-import LeftNavLayout from '@common/layouts/LeftNavLayout'
+import LeftNavLayout from '@common/layouts/LeftNavLayout';
+
+//stylesheets for index
+//import '../styles/css/custom.css';
+//import '../styles/css/bootstrap.min.css';
+//import '../stles/font-awesome-4.7.0/css/font-awesome.min.css';
+//import '../styles/css/aos.css';   
 
 // temporarily disabled: automatic redirect to login page if no session exists
 // export async function getServerSideProps({ req }) {
@@ -26,30 +31,25 @@ import LeftNavLayout from '@common/layouts/LeftNavLayout'
 const Home: NextPageWithLayout = () => {
   const {data: session} = useSession();
 
+
+
   return (
     <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Head>
-        <title>Home</title>
-      </Head>
 
-      <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 5 }}>
-        <Typography variant="h3" >Rodeo Co.</Typography>
-        <Typography variant="h5" >Plan, manage, and wrangle your rodeos with ease - all in one place!</Typography>
-        <img src={marketing3.src} alt="marketing1" width='600' height='350' /> 
-      </Container>
-
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', mb: 5 }}>
-          <img src={marketing1.src} alt="marketing1" width='600' height='350' /> 
-          <img src={marketing2.src} alt="marketing2" width='150' height='250' />
-      </Box>
-
-      <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 5 }}>
-        <Typography variant="h3" >Rodeo Co is a simple solution to your Rodeo planning needs, from entries to ranking calculations.</Typography>
-      </Container>
-
-      <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', mb: 5 }}>
-        <Typography variant="h6" sx={{ textAlign: 'cneter', mb: 5 }}>Rodeo Co. works with local youth rodeo clubs to ensure we have the best product possible.</Typography>
-        <img src={cubra.src} alt="cubra" width='200' height='150' />
+      <Container sx={{
+          margin: 'auto',
+          backgroundColor: 'white',
+          alignItems: 'center',
+          borderRadius: 'md',
+          width: '90%',
+          height: '90%',
+          gap: '4',
+          p: '3',
+        }}>
+          <a href='/rodeos'><img src={header.src} alt="header" width='100%' height='100%' /></a>
+          <img src={body.src} alt="body" width='100%' height='100%' />
+          <a href='/login'><img src={tradition.src} alt="tradition" width='100%' height='100%' /></a>
+          
       </Container>
 
       <Container>
