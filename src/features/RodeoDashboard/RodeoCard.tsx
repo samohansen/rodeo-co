dfe1278ac71914fa82fc3e73afad1feba4b12bf7
Grace Fillmore <gracefillmore8@gmail.com>
2023-03-28 13:09:36 -0600

    Store image URLs with rodeos (#25)
    * Store image URLs with rodeos
    - Update rodeo schema to include image URLs, reseed db
    - Add imgur to trusted hosts in nextjs config
    - Use rodeo’s stored image URL to populate cards
    - Add image options to rodeo form
    
    * Update layouts
    - remove root layout paddings
    - update rodeo dashboard tab layouts
    
    * Add `prisma generate` to build script, to account for schema changes

--- src/features/RodeoDashboard/RodeoCard.tsx
+++ src/features/RodeoDashboard/RodeoCard.tsx
@@ -2,16 +2,15 @@ import ActionAreaCard from "@common/dataDisplay/ActionAreaCard";
 import { formatDate } from "@common/utils";
 
 const RodeoCard = ({rodeo, onClick}) => {
-  const rodeoImages = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png']
   const {name, location, date} = rodeo
 
   return (
-    <ActionAreaCard 
+    <ActionAreaCard
       title={name}
       bodyLine1={location}
       bodyLine2={formatDate(date)}
       onClick={onClick}
-      imagePath={rodeoImages[Math.floor(Math.random() * 9)]}
+      imgSrc={rodeo.imgSrc || '/1.png'}
     />
   )
 }
